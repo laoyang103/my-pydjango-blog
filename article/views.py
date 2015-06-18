4958cdc42730673860e3d0335cf58b262a32d3e4
laoyang103 <553069938@qq.com>
2015-06-18 13:27:32 +0000

    dynamic url and access sqlite3

--- article/views.py
+++ article/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello Django')
+    
+def detail(request, num):
+    art = Article.objects.all()[int(num)]
+    return HttpResponse(art)

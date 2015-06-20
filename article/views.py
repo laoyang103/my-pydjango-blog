41554e8ac19798ec0994e826c8b6b8a7bbd0efb4
laoyang103 <553069938@qq.com>
2015-06-20 07:02:39 +0000

    Code highlighting with monokai.css

--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+from markdown import markdown
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from article.models import Article
@@ -6,10 +7,6 @@ from datetime import datetime
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello Django')
-    
-def detail(request, num):
-    art = Article.objects.all()[int(num)]
-    return HttpResponse(art)
 
 def test_template(request):
     return render(request, 'test.html', {'curr_time': datetime.now()})
